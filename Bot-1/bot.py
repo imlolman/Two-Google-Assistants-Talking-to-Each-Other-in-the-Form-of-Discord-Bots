d34a8046f9ad3816c9cea28a281afa221e1c842a
Satyam Gupta <satyam.gupta756@gmail.com>
2020-01-14 23:48:25 +0530

    Updated CODE

---
+++ Bot-1/bot.py
@@ -0,0 +1,41 @@
+# bot.py
+import os
+import subprocess
+import json
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+DEVICE_MODEL_ID = os.getenv('DEVICE_MODEL_ID')
+DEVICE_ID = os.getenv('DEVICE_ID')
+
+client = discord.Client()
+
+# @client.event
+# async def on_ready():
+#     for guild in client.guilds:
+#         if guild.name == GUILD:
+#             break
+
+#     print(
+#         f'{client.user} is connected to the following guild:\n'
+#         f'{guild.name}(id: {guild.id})\n'
+#     )
+
+#     members = '\n - '.join([member.name for member in guild.members])
+#     print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user or message.author.name == 'imLolman': # Replace With Your Discord Name
+        return
+
+    batcmd='python ../Assistant/talk.py --query "'+message.content+'" --device-model-id '+DEVICE_MODEL_ID+' --device-id '+ DEVICE_ID + ''
+    result = subprocess.check_output(batcmd)
+    with open('out.json', 'r') as in_f:
+        data = json.load(in_f)
+    await message.channel.send(data['string'])
+
+client.run(TOKEN)
---
+++ readme.md
@@ -0,0 +1,24 @@
+## Two Google Assistants Talking to Each Other in the Form of Discord Bots.
+### **Learning Steps:**
+- Learn to make Discord Bot.
+- **https://realpython.com/how-to-make-a-discord-bot-python/**
+
+
+- Learn to Use Google Assistant API using Python.
+- **https://troubleshooter.xyz/wiki/how-to-install-google-assistant-on-windows-10/**
+------------
+### **Steps to Build Discord Bots:**
+**- Follow https://realpython.com/how-to-make-a-discord-bot-python/**
+- Install all The PIP Requirements By Following Above Guide
+- Add Two Different Bot's Token in `Bot-1/.env` and `Bot-2/.env`
+- Create test Discord Server
+- Add GUILDs Too
+
+### **Steps to Build Assistant Backend:**
+**- Follow https://troubleshooter.xyz/wiki/how-to-install-google-assistant-on-windows-10/**
+- Install all The PIP Requirements By Following Above Guide
+- Copy Credentials From **Step 6** `credentials.json` to Assistant/credentials.json
+- Add Device Model IDs and Device IDs to `Bot-1/.env` and `Bot-2/.env`
+
+------------
+### Further You Can Customize Continued Conversations (Contextual Conversations) By Toggling True/False in Line 66 in `Assistant/talk.py`
